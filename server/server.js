5e5a8966974379e017d57d5bb75acbe8b29ff043
Jose Garcia <josegarciamanez@gmail.com>
2018-04-24 19:30:26 +0200

    crear schema bd

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 require('./config/config');
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -11,27 +13,17 @@ app.use(bodyParser.urlencoded({
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario')
-})
 
-app.post('/usuario', function(req, res) {
-    let body = req.body;
-    res.json({
-        persona: body
-    })
-})
+app.use(require('./routes/usuario'));
 
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id
-    });
-})
 
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario')
-})
+
+mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
+    if (err) throw err;
+    console.log('Base de datos ONLINE');
+
+});
+
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto: ', 3000);
