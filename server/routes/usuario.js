7bd874679b745b75b3861c58a20b1c0357b2f0c6
Jose Garcia <josegarciamanez@gmail.com>
2018-04-28 17:02:47 +0200

    app finalizada

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const bcrypt = require('bcrypt')
-const _ = require('underscore')
-const Usuario = require('../models/usuario')
+const express = require('express');
+const bcrypt = require('bcrypt');
+const _ = require('underscore');
+const Usuario = require('../models/usuario');
 
 const app = express()
 
@@ -12,7 +12,7 @@ app.get('/usuario', function(req, res) {
     let limite = req.query.limite || 5
     limite = Number(limite)
 
-    Usuario.find({})
+    Usuario.find({ estado: true }, 'nombre email role estado google img')
         .skip(desde)
         .limit(limite)
         .exec((err, usuarios) => {
@@ -23,9 +23,13 @@ app.get('/usuario', function(req, res) {
                 })
             }
 
-            res.json({
-                ok: true,
-                usuarios
+            Usuario.count({ estado: true }, (err, conteo) => {
+
+                res.json({
+                    ok: true,
+                    usuarios,
+                    cuantos: conteo
+                })
             })
         })
 })
@@ -72,8 +76,34 @@ app.put('/usuario/:id', function(req, res) {
     })
 })
 
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario')
+app.delete('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+    let cambiaEstado = {
+        estado: false
+    }
+
+    // Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+    Usuario.findByIdAndUpdate(id, cambiaEstado, { new: true }, (err, usuarioBorrado) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            })
+        };
+        if (!usuarioBorrado) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Usuario no encontrado'
+                }
+            })
+        };
+        res.json({
+            ok: true,
+            usuario: usuarioBorrado
+        })
+
+    })
 })
 
-module.exports = app
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-require('./config/config');
-const express = require('express');
-const mongoose = require('mongoose');
+require('./config/config')
+const express = require('express')
+const mongoose = require('mongoose')
 
-const app = express();
-const bodyParser = require('body-parser');
+const app = express()
+const bodyParser = require('body-parser')
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
@@ -13,19 +13,13 @@ app.use(bodyParser.urlencoded({
 // parse application/json
 app.use(bodyParser.json())
 
+app.use(require('./routes/usuario'))
 
-app.use(require('./routes/usuario'));
-
-
-
-mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
-    if (err) throw err;
-    console.log('Base de datos ONLINE');
-
-});
-
+mongoose.connect(process.env.URLDB, (err, res) => {
+    if (err) throw err
+    console.log('Base de datos ONLINE')
+})
 
 app.listen(process.env.PORT, () => {
-    console.log('Escuchando puerto: ', 3000);
-
+    console.log('Escuchando puerto: ', 3000)
 })
