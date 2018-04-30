59f8601ce8d6e04e29f966d27fa83a39c13eb96e
Jose Garcia <josegarciamanez@gmail.com>
2018-04-30 17:15:44 +0200

    pag inicio

--- server/server.js
+++ server/server.js
@@ -1,19 +1,23 @@
 require('./config/config')
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path');
 
 const app = express()
 const bodyParser = require('body-parser')
 
-// parse application/x-www-form-urlencoded
+// !parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
     extended: false
 }))
 
-// parse application/json
+// !parse application/json
 app.use(bodyParser.json())
 
-// Configuración global de rutas
+// !Habilitar la carpeta public
+app.use(express.static(path.resolve(__dirname, '../public/')));
+
+// !Configuración global de rutas
 app.use(require('./routes/index'));
 
 mongoose.connect(process.env.URLDB, (err, res) => {
